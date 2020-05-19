f6976e6dbd028bfdab9f3ccd79c7afb316caf9bc
kweku76 <daffran@gmail.com>
2020-05-19 20:09:49 +1000

    Update script.js

--- dom-array-methods/script.js
+++ dom-array-methods/script.js
@@ -24,5 +24,32 @@ async function getRandomUser() { //using fetch and async await
     money: Math.floor(Math.random() * 1000000)
   }
 
-  console.log(newUser);
+  addData(newUser);
 }
+// Add new obj to data array
+function addData(obj) {
+  data.push(obj); //using the push method to put something at the end of array
+
+  updateDom();
+}
+
+//Update DOM
+function updateDom(provideData = data) {
+  //clear the main div / replace
+  main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
+
+  provideData.forEach(item => {
+    const element = document.createElement('div'); // this variable will create a html element (div)
+    element.classList.add('person'); //adding a 'person' class to element variable
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+    main.appendChild(element); // using formatMoney function to change numbers into currency.
+  });
+
+  //Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+  function formatMoney(number) {
+    return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); //changes numbers to currency
+  }
+}
+
+//event listeners
+addUserBtn.addEventListener('click', getRandomUser); //when addUserBtn is clicked it will generate a new random user by getRandomUser function
